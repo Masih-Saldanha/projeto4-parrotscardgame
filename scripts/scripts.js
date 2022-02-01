e9b359db482b85c1767b16fca932396f2b518d15
Masih Saldanha Xavier <masih.saldanha@gmail.com>
2022-02-01 12:01:13 -0300

    Layout das cartas (costas) criado

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 let numeroCartas = undefined;
 function escolherNumeroCartas() {
-    numeroCartas = prompt("Com quantas cartas deseja jogar? (Qualquer número par entre 4 e 14)");
-    if (numeroCartas < 4 || numeroCartas > 14 || (numeroCartas % 2) !== 0) {
-        escolherNumeroCartas();
-    }
+    // numeroCartas = prompt("Com quantas cartas deseja jogar? (Qualquer número par entre 4 e 14)");
+    // if (numeroCartas < 4 || numeroCartas > 14 || (numeroCartas % 2) !== 0) {
+    //     escolherNumeroCartas();
+    // }
 }
