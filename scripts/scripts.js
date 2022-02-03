6d05f8e733bc2ad3a21f72b7feb7e92a66863b5f
Masih Saldanha Xavier <masih.saldanha@gmail.com>
2022-02-03 12:09:23 -0300

    Adicionada lógica de embaralhar e feitos alguns ajustes finos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,44 @@
-let renderizarCartas = undefined;
+let listaDeCartas = [];
+let renderizarCartas = document.querySelector('section');
 let numeroCartas = undefined;
 let virarFrente = undefined;
 let virarCostas = undefined;
 let cartasMarcadas = [];
-let contador = undefined;
+// FUNÇÃO DE ESCOLHER NÚMERO DE CARTAS, FAZER PARES E EMBARALHAR
+function escolherNumeroCartas() {
+    while (numeroCartas < 4 || numeroCartas > 14 || (numeroCartas % 2) !== 0) {
+    numeroCartas = prompt("Com quantas cartas deseja jogar? (Qualquer número par entre 4 e 14)");
+    }
+    // renderizarCartas = document.querySelector('section');
+	for(let i = 0; i < numeroCartas; i = i + 1) {
+		renderizarCartas.innerHTML += `
+        <figure class= "carta-${i}" data-identifier="card" onclick="virarCarta(${i})">
+            <div class="carta-frente carta-frente-${i} carta" data-identifier="front-face"><h2>FRENTE ${i}</h2></div>
+            <div class="carta-costas carta-costas-${i} carta" data-identifier="back-face"><h2>COSTAS ${i}</h2></div>
+        </figure>
+        `;
+        listaDeCartas.push(`${i}`);
+        if (parseInt(numeroCartas) === parseInt(listaDeCartas.length)) {
+            listaDeCartas.sort(comparador);
+            console.log(listaDeCartas);
+        }
+    }
+        renderizarCartas.innerHTML = "";
+        for (let n = 0; n < listaDeCartas.length; n++) {
+            renderizarCartas.innerHTML += `
+            <figure class= "carta-${listaDeCartas[n]}" data-identifier="card" onclick="virarCarta(${listaDeCartas[n]})">
+                <div class="carta-frente carta-frente-${listaDeCartas[n]} carta" data-identifier="front-face"><h2>FRENTE ${listaDeCartas[n]}</h2></div>
+                <div class="carta-costas carta-costas-${listaDeCartas[n]} carta" data-identifier="back-face"><h2>COSTAS ${listaDeCartas[n]}</h2></div>
+            </figure>
+            `;
+        }
+}
+escolherNumeroCartas();
+// FUNÇÃO PARA EMBARALHAR ARRAY DE CARTAS
+function comparador() {
+    return Math.random() - 0.5;
+}
+// FUNÇÃO PARA VIRAR AS CARTAS AO CLICK
 function virarCarta(numero) {
     virarFrente = document.querySelector(`.carta-frente-${numero}`);
     virarCostas = document.querySelector(`.carta-costas-${numero}`);
@@ -13,9 +48,6 @@ function virarCarta(numero) {
     if (cartasMarcadas[0] === cartasMarcadas[1]) {
         cartasMarcadas.splice(1);
     } 
-    // else {
-        // stop;
-    // }
     console.log(cartasMarcadas);
     if (cartasMarcadas.length >= 2) {
         // virarCarta().removeAttribute('onclick');
@@ -34,22 +66,3 @@ function virarCarta(numero) {
     }
 }
 
-function escolherNumeroCartas() {
-    while (numeroCartas < 4 || numeroCartas > 14 || (numeroCartas % 2) !== 0) {
-    numeroCartas = prompt("Com quantas cartas deseja jogar? (Qualquer número par entre 4 e 14)");
-    }
-	for(contador = 0; contador < numeroCartas; contador = contador + 1) {
-		renderizarCartas = document.querySelector('section');
-		renderizarCartas.innerHTML += `
-        <figure data-identifier="card" onclick="virarCarta(${contador})">
-            <div class="carta-frente carta-frente-${contador} carta" data-identifier="front-face"><h2>FRENTE ${contador}</h2></div>
-            <div class="carta-costas carta-costas-${contador} carta" data-identifier="back-face"><h2>COSTAS ${contador}</h2></div>
-        </figure>
-        `;
-	}
-    // MÉTODO ORIGINAL
-    // numeroCartas = prompt("Com quantas cartas deseja jogar? (Qualquer número par entre 4 e 14)");
-    // if (numeroCartas < 4 || numeroCartas > 14 || (numeroCartas % 2) !== 0) {
-    //     escolherNumeroCartas();
-    // }
-}
