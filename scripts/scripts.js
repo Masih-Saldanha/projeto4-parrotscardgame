c2848e5d04d72f8a464c694a30c8bfd359c189e6
Masih Saldanha Xavier <masih.saldanha@gmail.com>
2022-02-05 01:39:31 -0300

    Implementada marcação de cartas viradas

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,6 +5,10 @@ let virarFrente = undefined;
 let virarCostas = undefined;
 let cartasMarcadas = [];
 let teste = undefined;
+let paresEncontrados = 0;
+// let carta1 = undefined;
+// let carta2 = undefined;
+let jogadas = 0;
 // poderia também, botar todos os nomes das imagens num array,
 // e chamar eles com os índices dos números baralhados...
 // tipo... nome = nomes[indicesBaralhados[i]]
@@ -16,8 +20,6 @@ function escolherNumeroCartas() {
     while (numeroCartas < 4 || numeroCartas > 14 || (numeroCartas % 2) !== 0) {
         numeroCartas = prompt("Com quantas cartas deseja jogar? (Qualquer número par entre 4 e 14)");
     }
-    // renderizarCartas = document.querySelector('section');
-    // for (let n = 0; n < 2; n++) {
     for (let i = 0; i < numeroCartas; i = i + 1) {
         renderizarCartas.innerHTML += `
         <figure class= "carta-pai carta-${i}" data-identifier="card" onclick="virarCarta(${i})">
@@ -31,21 +33,8 @@ function escolherNumeroCartas() {
         listaDeCartas.push(imagem);
     }
     listaDeCartas.sort(comparador);
-    // console.log(renderizarCartas);
-    // if (parseInt(numeroCartas) === parseInt(listaDeCartas.length)) {
-    //     listaDeCartas.sort(comparador);
-    //     console.log(listaDeCartas);
-    // }
-
-    // console.log(imagem);
-    // console.log(listaDeCartas);
     renderizarCartas.innerHTML = "";
     for (let i = 0; i < listaDeCartas.length; i++) {
-        // teste = listaDeCartas[i].innerHTML;
-        // console.log(teste);
-        // teste = renderizarCartas.querySelector(`.carta-${i}`);
-        // let teste2 = `${listaDeCartas[i]}`;
-        // console.log(teste);
         renderizarCartas.innerHTML += `${listaDeCartas[i]}`;
     }
 }
@@ -60,25 +49,58 @@ function virarCarta(numero) {
     virarCostas = document.querySelector(`.carta-costas-${numero}`);
     virarFrente.classList.add("virar-frente");
     virarCostas.classList.add("virar-costas");
+    
     cartasMarcadas.push(numero);
-    if (cartasMarcadas[0] === cartasMarcadas[1]) {
+    if ((cartasMarcadas[0]) === (cartasMarcadas[1])) {
+        [i % (numeroCartas / 2)]
         cartasMarcadas.splice(1);
     }
     console.log(cartasMarcadas);
-    if (cartasMarcadas.length >= 2) {
-        // virarCarta().removeAttribute('onclick');
-        let cartaFrente1 = document.querySelector(`.carta-frente-${cartasMarcadas[0]}`);
-        let cartaFrente2 = document.querySelector(`.carta-frente-${cartasMarcadas[1]}`);
-        let cartaCostas1 = document.querySelector(`.carta-costas-${cartasMarcadas[0]}`);
-        let cartaCostas2 = document.querySelector(`.carta-costas-${cartasMarcadas[1]}`);
+
+    // carta100 = renderizarCartas.children[(cartasMarcadas[0]%7)].children[0].children[1].classList.value;
+    carta200 = renderizarCartas.children[(cartasMarcadas[1]%7)].children[0].children[1].classList.value; //POR ALGUM MOTIVO BUGA SE TIRAR ISSO
+    // console.log(carta1);
+    // console.log(carta2);
+    // if (carta1 === carta2) {
+
+    // }
+    if (cartasMarcadas.length >= 2 && renderizarCartas.children[cartasMarcadas[0]%(numeroCartas/2)].children[0].children[1].classList.value === renderizarCartas.children[cartasMarcadas[1]%(numeroCartas/2)].children[0].children[1].classList.value) {
+        // document.getElementById("virarCarta(1)")
+        // renderizarCartas.children[cartasMarcadas[0]].setAttribute('onclick', '');
+        // renderizarCartas.children[cartasMarcadas[1]].setAttribute('onclick', '');
+        console.log(cartasMarcadas);
+        document.querySelector(`.carta-${cartasMarcadas[0]}`).setAttribute('onclick', '');
+        document.querySelector(`.carta-${cartasMarcadas[1]}`).setAttribute('onclick', '');
+        // document.querySelectorAll(`section figure`)[cartasMarcadas[0]].setAttribute('onclick', '');
+        // document.querySelectorAll(`section figure`)[cartasMarcadas[1]].setAttribute('onclick', '');
+        // paresEncontrados++;
+        jogadas++;
+        // console.log(paresEncontrados);
+        console.log(jogadas);
         cartasMarcadas = [];
-        setTimeout(() => {
-            cartaFrente1.classList.remove("virar-frente");
-            cartaFrente2.classList.remove("virar-frente");
-            cartaCostas1.classList.remove("virar-costas");
-            cartaCostas2.classList.remove("virar-costas");
-            // virarCarta().addAttribute('onclick');
-        }, 1000);
+        // console.log(cartasMarcadas);
+    } else {
+        desvirarAutomático();
+        // console.log(paresEncontrados);
+        jogadas++;
+        console.log(jogadas);
+        cartasMarcadas = [];
     }
+}
+// FUNÇÃO PARA DESVIRAR CARTAS AUTOMATICAMENTE
+function desvirarAutomático() {
+    // virarCarta().removeAttribute('onclick');
+    let cartaFrente1 = document.querySelector(`.carta-frente-${cartasMarcadas[0]}`);
+    let cartaFrente2 = document.querySelector(`.carta-frente-${cartasMarcadas[1]}`);
+    let cartaCostas1 = document.querySelector(`.carta-costas-${cartasMarcadas[0]}`);
+    let cartaCostas2 = document.querySelector(`.carta-costas-${cartasMarcadas[1]}`);
+    cartasMarcadas = [];
+    setTimeout(() => {
+        cartaFrente1.classList.remove("virar-frente");
+        cartaFrente2.classList.remove("virar-frente");
+        cartaCostas1.classList.remove("virar-costas");
+        cartaCostas2.classList.remove("virar-costas");
+        // virarCarta().addAttribute('onclick');
+    }, 1000);
 }
 
